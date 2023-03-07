ab48e2b121e4e95bcd2e2db31125eea2bbce1b6e
Daniil Stasulevich <alexgrist133@gmail.com>
2023-03-07 11:41:47 +0300

    implement search form, add more fields to repository block

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,49 @@
-const token = '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';
-
 const form = document.forms.reps;
 const searchInput = form.elements.search;
-const searchBtn = form.elements.button;
 const repositories = document.querySelector('.repositories-container');
-
 
 form.addEventListener("submit",appendRepositories);
 
 function appendRepositories(){
 
-    let repName = getRepositories(searchInput.value)
+    getRepositories(searchInput.value)
         .then(repos => {
-            console.log(repos);
-            for(let item of repos){
-                const linkToGithub = document.createElement('a');
-                linkToGithub.innerText = item.name;
-                linkToGithub.href = item.clone_url;
-                linkToGithub.setAttribute("target","_blank");
-                repositories.append(linkToGithub);
+            if(repos.length === 0 ){
+                repositories.innerHTML = "Nothing found";
+            }
+            else {
+                repositories.innerHTML = '';
+                for (let item of repos) {
+                    const repoContainer = document.createElement('div');
+                    const linkToGithub = document.createElement('a');
+                    const repoDescription = document.createElement('p');
+                    const repoLanguage = document.createElement('span');
+
+                    repoDescription.innerText = item.description;
+                    repoLanguage.innerText = item.language;
+
+                    repoContainer.classList.add('repository-content')
+                    repoLanguage.classList.add('language');
+                    linkToGithub.classList.add('link');
+                    repoDescription.classList.add('description');
+
+                    linkToGithub.innerText = item.name;
+                    linkToGithub.href = item.clone_url;
+                    linkToGithub.setAttribute("target", "_blank");
+
+                    repoContainer.append(linkToGithub);
+                    repoContainer.append(repoDescription);
+                    repoContainer.append(repoLanguage);
+
+                    repositories.append(repoContainer);
+                }
             }
         })
-        .catch(err => console.log(err));
-    const repositoryName = document.createElement('div');
-
-    // repositoryName.append(repName[0].name);
-    // repositories.append(repositoryName);
+        .catch(err => alert(err));
 }
 
 async function getRepositories(repoName) {
-    const response = await fetch(`https://api.github.com/search/repositories?q=${repoName}&per_page=10`,
-        {
-            headers:{
-                Authorization: `Bearer ${token}`,
-            }
-        });
+    const response = await fetch(`https://api.github.com/search/repositories?q=${repoName}&per_page=10`);
     const data = await response.json();
     return data.items;
 }
